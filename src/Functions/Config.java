a283d4d02f8c0531ad07bf4ec54c92dd029b8bd2
Hosam <97047710+ItzEnigma@users.noreply.github.com>
2022-05-08 17:11:58 +0200

    initial ver

---
+++ src/Functions/Config.java
@@ -0,0 +1,11 @@
+
+package Functions;
+
+public class Config {
+    private static String portNumber = "1527";
+    private static String dbName = "myleague";
+
+    public static String hostName = "jdbc:derby://localhost:" +portNumber+"/" + dbName;
+    public static String username = "root" ;
+    public static String password = "password" ;
+}
