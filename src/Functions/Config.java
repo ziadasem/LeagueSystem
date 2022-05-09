db28dd52f5c94f7839e45d26c66d56b918e3c210
RamyMohamed47 <mohamedramy228@ymail.com>
2022-05-09 23:09:06 +0200

    Update

--- src/Functions/Config.java
+++ src/Functions/Config.java
@@ -3,8 +3,8 @@ package Functions;
 
 public class Config {
     private static String portNumber = "1527";
-    private static String dbName = "myleague";
-
+    private static String dbName = "myleauge";
+    public static String adminID = "1234";
     public static String hostName = "jdbc:derby://localhost:" +portNumber+"/" + dbName;
     public static String username = "root" ;
     public static String password = "password" ;
